538564c18912e516ecb16d1599acf3971bf0436f
Andy Tockman <andy@tck.mn>
2019-04-24 17:23:02 -0500

    add gpl header to a bunch of files

--- src/main/cpp/semitone-native.cpp
+++ src/main/cpp/semitone-native.cpp
@@ -1,3 +1,21 @@
+/*
+ * Semitone - tuner, metronome, and piano for Android
+ * Copyright (C) 2019  Andy Tockman <andy@tck.mn>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <oboe/Oboe.h>
 #include "mn_tck_semitone_PianoEngine.h"
 #include "PianoEngine.h"
